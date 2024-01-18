5afdfacfd60a754523262caa3292690875ff992b
Aditi shivaji Jagzape <102456204+AditiJagzape@users.noreply.github.com>
2024-01-18 21:14:40 +0530

    Add files via upload
    SpringBoot School Management System CRUD operations code

---
+++ SpringSchoolManagementProjectApplication.java
@@ -0,0 +1,13 @@
+package com.springSchoolManagementProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSchoolManagementProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSchoolManagementProjectApplication.class, args);
+	}
+
+}
